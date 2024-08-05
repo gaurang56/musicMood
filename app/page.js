04a728b74baf884c47cb0ed9c1e374391782ada9
gaurang56 <gaurang.deka@gmail.com>
2024-08-05 20:24:47 +0400

    first commit

---
+++ app/page.js
@@ -0,0 +1,28 @@
+// app/page.js
+'use client'
+
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import Problem from '@/components/Problem'
+import Features from '@/components/Features'
+import Plans from '@/components/Plans'
+import FAQ from '@/components/FAQ'
+import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="pt-16">
+        <Hero />
+        <Problem />
+        <Features />
+        <Plans />
+        <FAQ />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  )
+}
